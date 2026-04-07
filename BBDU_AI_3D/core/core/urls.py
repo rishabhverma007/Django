d0bd48ffa5d40cdafaf57ce8a1a45fb50b32e253
Rishabh Verma <rishabh300verma@gmail.com>
2026-04-07 12:00:38 +0530

    practice

--- BBDU_AI_3D/core/core/urls.py
+++ BBDU_AI_3D/core/core/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import Accounts
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("Accounts/",Accounts.urls),
 ]
