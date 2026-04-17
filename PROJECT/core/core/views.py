1af493e24ece2b054d66671aa3c96be68afa3f90
Rishabh Verma <rishabh300verma@gmail.com>
2026-04-17 20:01:14 +0530

    new

---
+++ PROJECT/core/core/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,"")
+
+def about(request):
+    return render(request,"")
+
+def contact(request):
+    return render(request,"")
