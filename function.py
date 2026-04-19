1c62269154667548dc79051ae4d1b045c5373fb8
Rishabh Verma <rishabh300verma@gmail.com>
2026-04-19 08:27:23 +0530

    new

--- function.py
+++
@@ -1,7 +0,0 @@
-import allfunction
-print(allfunction.add(5, 3))
-print(allfunction.subtract(5, 3))
-print(allfunction.multiply(5, 3))
-print(allfunction.divide(5, 3))  
-print(allfunction.square(4))
-print(allfunction.cube(4))
